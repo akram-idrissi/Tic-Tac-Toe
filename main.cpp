551c4f741f68d48e0ea0ad903d3924ba9d0501d1
akram-idrissi <akram.mo.gb@gmail.com>
2022-12-02 22:17:57 +0100

    Added the displayBoard function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 
+const int board[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+void displayBoard(const int* board) {
+    std::cout << board[0] << " | "  << board[1] << " | "  << board[2] << std::endl;
+    std::cout << "---------" << std::endl;
+    std::cout << board[3] << " | "  << board[4] << " | "  << board[5] << std::endl;
+    std::cout << "---------" << std::endl;
+    std::cout << board[6] << " | "  << board[7] << " | "  << board[8] << std::endl << std::endl;
+}
+
 int main() {
 
-    
     return 0;
 }

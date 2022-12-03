f1e374f2d3f20e37d01c1cecdc6c44939d9af374
akram-idrissi <akram.mo.gb@gmail.com>
2022-12-03 18:14:18 +0100

    Implemented the displayBoard function

--- main.cpp
+++ main.cpp
@@ -55,3 +55,13 @@ void instructions() {
     std::cout << "The numbers corresponds to the emsi desired board position, as illustrated: \n\n";
 }
 
+void displayBoard(const char* board) {
+    std::cout << board[0] << " | " << board[1] << " | " << board[2] << std::endl;
+    std::cout << "---------";
+    std::cout << board[3] << " | " << board[4] << " | " << board[5] << std::endl;
+    std::cout << "---------";
+    std::cout << board[6] << " | " << board[7] << " | " << board[8] << std::endl;
+
+
+}
+

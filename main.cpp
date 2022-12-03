4e2c1de4ed6110c0a402b38bb3f2a0aa16aedc26
akram-idrissi <akram.mo.gb@gmail.com>
2022-12-03 13:40:52 +0100

    Added prototypes of functions

--- main.cpp
+++ main.cpp
@@ -2,41 +2,21 @@
 
 const int board[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-void displayBoard(const int* board) {
-    std::cout << board[0] << " | "  << board[1] << " | "  << board[2] << std::endl;
-    std::cout << "---------" << std::endl;
-    std::cout << board[3] << " | "  << board[4] << " | "  << board[5] << std::endl;
-    std::cout << "---------" << std::endl;
-    std::cout << board[6] << " | "  << board[7] << " | "  << board[8] << std::endl << std::endl;
-}
+void instructions();
+char askYesNo(std::string question);
+int askNumber(std::string question, int high, int low = 0);
+char humanPiece();
+char opponent(char piece);
+void displayBoard(const char* board[]);
+char winner(const char* board[]);
+bool isLegal(const char* board[], int move);
+int humanMove(const char* board[], char human);
+int computerMove(const char* board[], char computer);
+void announceWinner(char winner, char computer, char human);
 
-void welcomeMessage() {
-    std::cout << "\n\n";
-    std::cout << "------ Welcome to the Emsi mini project - Tic Tac Toe game ------ \n\n";
-    std::cout << "Make your move known by entering a number, 0 - 8. \n";
-    std::cout << "The numbers corresponds to the emsi desired board position, as illustrated: \n\n";
-}
-
-char askForAns(std::string question) {
-    char answer = '\0';
-    do {
-        std::cout << "";
-    } while(answer != 'y' && answer != 'n' );
-
-    return answer;
-}
-
-int askForNumber() {
-    int num = -1;
-
-    do {
-        std::cout << "";
-    } while(num < 0 || num > 8 );
-
-    return num;
-}
 
 int main() {
 
     return 0;
 }
+

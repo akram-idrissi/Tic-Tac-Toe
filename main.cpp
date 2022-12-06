df311f9529f36923e426b2b7397201cc16c4cd1b
akram-idrissi <akram.mo.gb@gmail.com>
2022-12-06 11:42:56 +0100

    Updated the displayBoard function

--- main.cpp
+++ main.cpp
@@ -57,11 +57,11 @@ void instructions() {
 }
 
 void displayBoard(const char* board) {
-    std::cout << board[0] << " | " << board[1] << " | " << board[2] << std::endl;
-    std::cout << "---------";
-    std::cout << board[3] << " | " << board[4] << " | " << board[5] << std::endl;
-    std::cout << "---------";
-    std::cout << board[6] << " | " << board[7] << " | " << board[8] << std::endl;
+    std::cout << "  " << board[0] << " | " << board[1] << " | " << board[2] << std::endl;
+    std::cout << "  " << "---------" << std::endl;
+    std::cout << "  " << board[3] << " | " << board[4] << " | " << board[5] << std::endl;
+    std::cout << "  " << "---------" << std::endl;
+    std::cout << "  " << board[6] << " | " << board[7] << " | " << board[8] << std::endl;
 }
 
 void announceWinner(char winner, char computer, char human) {
